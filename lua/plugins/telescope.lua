f23ecd02871c94173eaee55b71242c2204721d00
Malachi <malachi.fraenkel@gmail.com>
2023-08-21 00:00:02 +0300

    Revert 1 commits
    ec567a8 'Revert "replaced undotree with telescope-undo"'

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,8 +6,7 @@ return {
         'nvim-telescope/telescope-ui-select.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim',
             build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-        },
-        "debugloop/telescope-undo.nvim",
+        }
     },
 
     config = function()
@@ -23,13 +22,7 @@ return {
                         ["<C-k>"] = actions.move_selection_previous,
                         ["<C-j>"] = actions.move_selection_next,
                     }
-                },
-                layout_strategy = "vertical",
-                layout_config = {
-                    preview_cutoff = 30,
-                    preview_height = 0.65,
-                    prompt_position = 'bottom',
-                },
+                }
             },
             pickers = {},
             extensions = {
@@ -44,19 +37,10 @@ return {
                         -- even more opts
                     }
                 },
-                undo = {
-                    side_by_side = true,
-                    layout_strategy = "vertical",
-                    layout_config = {
-                        preview_cutoff = 0,
-                        preview_height = 0.8,
-                    },
-                },
             }
         })
         telescope.load_extension("fzf")
         telescope.load_extension("ui-select")
-        telescope.load_extension("undo")
 
 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'files', noremap = true, silent = true })
@@ -64,5 +48,6 @@ return {
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end, { desc = 'grep string', noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", { desc = 'undo', noremap = true, silent = true })
     end,
 }
