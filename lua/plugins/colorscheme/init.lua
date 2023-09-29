a03ec967ec148b418f7c7ab881faa59bec1f4786
Malachi Fraenkel <malachi.fraenkel@gmail.com>
2023-09-29 23:02:49 +0300

    better importing of subfolders in the plugins directory

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -1,15 +1,3 @@
 return {
-    {
-        'folke/tokyonight.nvim',
-        lazy = false,
-        priority = 1000,
-        config = function() require('plugins.colorscheme.tokyonight') end,
-    },
-
-    {
-        'catppuccin/nvim',
-        lazy = false,
-        priority = 1000,
-        config = function() require('plugins.colorscheme.catppuccin') end,
-    },
+    { import = 'plugins.colorscheme' },
 }
