c274ab2a0bcb84f24182ee3dcac060ef9d311111
Malachi Fraenkel <malachi.fraenkel@gmail.com>
2023-09-29 18:07:31 +0300

    made inddent-blankline scope the same color as rainbow-delitmiters

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -10,13 +10,23 @@ return {
     },
 
     config = function()
+        local highlights = {
+            'RainbowDelimiterRed',
+            'RainbowDelimiterYellow',
+            'RainbowDelimiterBlue',
+            'RainbowDelimiterOrange',
+            'RainbowDelimiterGreen',
+            'RainbowDelimiterViolet',
+            'RainbowDelimiterCyan',
+        }
+
         require('ibl').setup({
             scope = {
                 enabled = true,
                 show_start = true,
                 show_end = true,
                 injected_languages = true,
-                highlight = { 'Function', 'Label' },
+                highlight = highlights,
             },
             exclude = {
                 filetypes = {
