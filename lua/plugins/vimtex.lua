75510cb55d49ef57788885fd60a5d8988efdb915
Malachi Fraenkel <malachi.fraenkel@gmail.com>
2024-10-20 21:07:32 +0300

    fixed sumatraPDF vimtex inverse search

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,7 +1,9 @@
 return {
 
     'lervag/vimtex',
-    lazy = false, -- we don't want to lazy load VimTeX
+    -- lazy = false, -- we don't want to lazy load VimTeX
+
+    ft = { 'tex' },
 
     init = function()
         -- VimTeX configuration goes here
@@ -9,5 +11,21 @@ return {
         vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
         vim.g.vimtex_syntax_enabled = 0
         -- vim.g.vimtex_view_general_options_latexmk = '-reuse-instance'
+
+        -- When working in a multi-file project, initiating inverse search (see
+        -- |vimtex-synctex-inverse-search|) may require opening a file that is not
+        -- currently open in a window. This option controls the command that is used to
+        -- open files as a result of an inverse search.
+        vim.g.vimtex_view_reverse_search_edit_cmd = 'edit'
+
+        -- Focus the terminal after inverse search
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'VimtexEventViewReverse',
+            group = au_group,
+            command = 'call b:vimtex.viewer.xdo_focus_vim()',
+        })
+
+        -- Place the following setting in sumatraPDF's advanced settings for inverse search to work:
+        -- InverseSearchCmdLine = cmd /q /c start /min "" powershell -WindowStyle Hidden -Command "& {$file = (('%f' -replace '\\','/') -replace '//wsl.localhost.Ubuntu-20.04',''); wsl --exec nvim --server /tmp/nvim.pipe --remote-send """":e $file | %l<CR>""""} %*"
     end,
 }
